3618c8acb9ae84dcff2f723472ef03b5adb26a16
Apurba Khanra <apurbakhanra09@gmail.com>
2022-09-19 10:19:45 +0530

    initial commit

---
+++ CodeQuit.java
@@ -0,0 +1,10 @@
+public class CodeQuit {
+    public void out()
+  {
+    System.out.println("\n*****************************************");
+    System.out.println("Thanks You For Using my Software");
+    System.out.println("*****************************************");
+    System.out.println("\t\tBy Apurba Khanra\n");
+    System.exit(0);
+  }
+}
---
+++ MainMenu.java
@@ -0,0 +1,18 @@
+public class MainMenu {
+    
+    public void menu()
+  {
+    System.out.println("\t\t*******************************************");
+    System.out.println("\t\t\t  EMPLOYEE MANAGEMENT SYSTEM");
+    System.out.println("\t\t*******************************************");
+    System.out.println("\t\t\t    --------------------");
+    System.out.println("\t\t\t     Apurba Khanra");
+    System.out.println("\t\t\t    --------------------");
+    System.out.println("\n\n1 . Add Employee Details");
+    System.out.println("2 . View Employee");
+    System.out.println("3 . Remove Employee");
+    System.out.println("4 . Update Employee");
+    System.out.println("5 . Quit");
+
+  }
+}
